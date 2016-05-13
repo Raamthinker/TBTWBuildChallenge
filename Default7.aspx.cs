72adb9ea1f18b82e4680af233fad9c3f4006d764
Raamthinker <ram007prabhu@gmail.com>
2016-05-13 19:54:50 +0530

    Create Default7.aspx.cs

---
+++ Default7.aspx.cs
@@ -0,0 +1,108 @@
+<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default7.aspx.cs" Inherits="Default7" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>Spikyflower by TEMPLATED</title>
+<link href="http://fonts.googleapis.com/css?family=Abel|Arvo" rel="stylesheet" type="text/css" />
+<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+<script language="javascript" type="text/javascript">
+// <!CDATA[
+
+
+
+function TABLE1_onclick() {
+
+}
+
+// ]]>
+</script>
+</head>
+<body>
+<div id="wrapper">
+<div id="header">
+		<div id="logo">
+			<h1><a href="#">KNOWLEDGE AND INFORMATION USING MACHINE LEARNING</a></h1>
+		</div>
+	</div>
+	<div id="page">
+		<div id="content">
+			<div class="post">
+    <form id="form2" runat="server">
+   <table border="12" id="TABLE1" onclick="return TABLE1_onclick()">
+   <tr><th colspan ="2">EDIT PROFILE</th>
+   </tr>
+   <tr><th>USER NAME</th><th style="width: 260px">
+       <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox></th></tr>
+       <tr><th>PASSWORD</th><th style="width: 260px">
+           <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox></th></tr>
+           <tr><th>CONFIRM PASSWORD</th><th style="width: 260px">
+               <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox></th></tr>
+               <tr><th>ADDRESS</th><th style="width: 260px">
+                   <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox></th></tr>
+                   <tr><th>CONTACT</th><th style="width: 260px">
+                       <asp:TextBox ID="TextBox5" runat="server"></asp:TextBox></th></tr>
+                       <tr><th>EMAIL</th><th style="width: 260px">
+                           <asp:TextBox ID="TextBox6" runat="server"></asp:TextBox></th></tr>
+                           <tr><th colspan="2">
+                               <asp:Button ID="Button1" runat="server" Text="UPDATE" OnClick="Button1_Click" />
+                               <asp:Button ID="Button2" runat="server" Text="BACK" OnClick="Button2_Click" /><asp:Button
+                                   ID="Button3" runat="server" Text="SELECT" OnClick="Button3_Click" /></th></tr>
+                               
+   </table>
+   </form>
+</div>
+		</div>
+		<div id="sidebar">
+			<ul>
+				<li>
+					<h2>Categories</h2>
+				</li>
+						<p>
+                            EDUCATION PLUS</p>
+                <p>
+                    POLITICAL NEWS</p>
+                <p>
+                    CRIME</p>
+                <p>
+                    ARTICLE</p>
+                <p>
+                    SPORT</p>
+                <p>
+                    MEDIA AND THIER EVENTS</p>
+						<li></li>
+				<li>
+					<h2>
+                        MARKET REport</h2>
+				</li>
+			</ul>
+            <p>
+                &nbsp;</p>
+            <p>
+                GOLD &nbsp; &nbsp; &nbsp; 24ct(1 gm):2,707.00rs</p>
+            <p>
+                SILVER &nbsp; &nbsp; Bar(1 kg):37.00rs</p>
+            <p>
+                EXCHANGE&nbsp; US$:61.88rs Euro:70.28</p>
+		</div>
+		
+	</div>
+	
+
+    
+
+
+
+
+</div>
+<div id="footer">
+	<p>
+        www.facebook.com/decannews.....</p>
+</div>
+<!-- end #footer -->
+</body>
+</html>
