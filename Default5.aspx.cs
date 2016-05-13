4ddb3348dc60144c0e9e94e8bc9aef5b5e9d72c0
Raamthinker <ram007prabhu@gmail.com>
2016-05-13 19:52:25 +0530

    Create Default5.aspx.cs

---
+++ Default5.aspx.cs
@@ -0,0 +1,102 @@
+<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default5.aspx.cs" Inherits="Default5" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>Spikyflower by TEMPLATED</title>
+<link href="http://fonts.googleapis.com/css?family=Abel|Arvo" rel="stylesheet" type="text/css" />
+<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+<script language="javascript" type="text/javascript">
+// <!CDATA[
+
+function TABLE1_onclick() {
+
+}
+
+
+
+// ]]>
+</script>
+</head>
+<body>
+<div id="wrapper">
+<div id="header">
+		<div id="logo">
+			<h1><a href="#">KNOWLEDGE AND INFORMATION USING MACHINE LEARNING</a></h1>
+		</div>
+	</div>
+	<div id="page">
+		<div id="content" >
+			<div class="post">
+			<form id="form2" runat="server">
+		<table border="12" id="TABLE1" style="width: 341px; height: 120px">
+    
+        <tr><th><asp:RadioButtonList ID="RadioButtonList1" runat="server" 
+                AutoPostBack="True" OnSelectedIndexChanged="RadioButtonList1_SelectedIndexChanged1"
+            RepeatDirection="Horizontal"  BackColor="Fuchsia" ForeColor="#0033CC">
+             <asp:ListItem>EDIT PROFILE</asp:ListItem>
+            <asp:ListItem>UPLOAD FILE</asp:ListItem>
+        </asp:RadioButtonList></th></tr>
+
+    
+        <tr><th><asp:Button ID="Button1" runat="server" OnClick="Button1_Click"  Text="Login" BackColor="Fuchsia" /></th></tr>
+            </table> 
+        
+		
+			
+    
+     
+    
+    </form> 
+    
+</div>
+		</div>
+		<div id="sidebar">
+			<ul>
+				<li>
+					<h2>Categories</h2>
+				</li>
+						<p>
+                            EDUCATION PLUS</p>
+                <p>
+                    POLITICAL NEWS</p>
+                <p>
+                    CRIME</p>
+                <p>
+                    ARTICLE</p>
+                <p>
+                    SPORT</p>
+                <p>
+                    MEDIA AND THIER EVENTS</p>
+						<li></li>
+				<li>
+					<h2>
+                        MARKET REport</h2>
+				</li>
+			</ul>
+            <p>
+                &nbsp;</p>
+            <p>
+                GOLD &nbsp; &nbsp; &nbsp; 24ct(1 gm):2,707.00rs</p>
+            <p>
+                SILVER &nbsp; &nbsp; Bar(1 kg):37.00rs</p>
+            <p>
+                EXCHANGE&nbsp; US$:61.88rs Euro:70.28</p>
+		</div>
+		
+	</div>
+</div>
+<div id="footer">
+	<p>
+        www.facebook.com/decannews.....</p>
+</div>
+<!-- end #footer -->
+
+
+    
+</body>
+</html>
